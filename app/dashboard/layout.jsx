4bc17021a04318e4ed6b9394fd5923a9f2565326
RowdyKGZ <rowdykg@gmail.com>
2024-01-29 14:38:28 +0600

    folder setup

---
+++ app/dashboard/layout.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import styles from "../_ui/dashboard/dashboard.module.css";
+import Sidebar from "../_ui/dashboard/sidebar/sidebar";
+import Navbar from "../_ui/dashboard/navbar/navbar";
+
+const DashboardLayout = ({ children }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.menu}>
+        <Sidebar />
+      </div>
+
+      <div className={styles.content}>
+        <Navbar />
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
