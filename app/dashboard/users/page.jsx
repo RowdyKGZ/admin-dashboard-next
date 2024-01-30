7cb5351556119d11da753e817a2179d8fcf0608f
RowdyKGZ <rowdykg@gmail.com>
2024-01-30 16:20:09 +0600

    user and product page

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,7 +1,103 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+import Search from "../../_ui/dashboard/search/search";
+import styles from "../../_ui/dashboard/users/users.module.css";
+import Pagination from "../../_ui/dashboard/pagination/pagination";
 
 const UsersPage = () => {
-  return <div>UsersPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for user..." />
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Created At</td>
+            <td>Role</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt="avatar"
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+
+            <td>johndoe@mail.com</td>
+            <td>2020.20.20</td>
+            <td>Clinet</td>
+            <td>Active</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <button className={`${styles.button} ${styles.delete}`}>
+                  Delete
+                </button>
+              </div>
+            </td>
+          </tr>
+
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt="avatar"
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+
+            <td>johndoe@mail.com</td>
+            <td>2020.20.20</td>
+            <td>Clinet</td>
+            <td>Active</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <button className={`${styles.button} ${styles.delete}`}>
+                  Delete
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <Pagination />
+    </div>
+  );
 };
 
 export default UsersPage;
